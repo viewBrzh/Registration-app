cf91460d8b672adb7f008e0f3ddac1c7e974266b
Hanafi Yaming <142217119+jhin3tytwoo@users.noreply.github.com>
2024-03-09 13:44:49 +0700

    footer update

--- registration-app/src/components/footer.js
+++ registration-app/src/components/footer.js
@@ -1,54 +1,106 @@
+import { Link } from "react-router-dom";
 function Footer() {
-    return (
-        <footer>
-
-            <div class="container-fluid bg-dark footer mt-5 py-5 wow fadeIn" data-wow-delay="0.1s">
-                <div class="container py-5">
-                    <div class="row g-5">
-                        <div class="col-lg-3 col-md-6">
-                            <h4 class="text-primary mb-4">Our Office</h4>
-                            <p class="mb-2"><i class="fa fa-map-marker-alt text-primary me-3"></i>123 Street, New York, USA</p>
-                            <p class="mb-2"><i class="fa fa-phone-alt text-primary me-3"></i>+012 345 67890</p>
-                            <p class="mb-2"><i class="fa fa-envelope text-primary me-3"></i>info@example.com</p>
-                            <div class="d-flex pt-3">
-                                <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-youtube"></i></a>
-                                <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-linkedin-in"></i></a>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-6">
-                            <h4 class="text-primary mb-4">Quick Links</h4>
-                            <a class="btn btn-link" href="">About Us</a>
-                            <a class="btn btn-link" href="">Contact Us</a>
-                            <a class="btn btn-link" href="">Our Services</a>
-                            <a class="btn btn-link" href="">Terms & Condition</a>
-                            <a class="btn btn-link" href="">Support</a>
-                        </div>
-                        <div class="col-lg-3 col-md-6">
-                            <h4 class="text-primary mb-4">Business Hours</h4>
-                            <p class="mb-1">Monday - Friday</p>
-                            <h6 class="text-light">09:00 am - 07:00 pm</h6>
-                            <p class="mb-1">Saturday</p>
-                            <h6 class="text-light">09:00 am - 12:00 pm</h6>
-                            <p class="mb-1">Sunday</p>
-                            <h6 class="text-light">Closed</h6>
-                        </div>
-                        <div class="col-lg-3 col-md-6">
-                            <h4 class="text-primary mb-4">Newsletter</h4>
-                            <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-                            <div class="position-relative w-100">
-                                <input class="form-control bg-transparent w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email"/>
-                                    <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <footer>
+      <div
+        className="container-fluid bg-dark footer mt-5 py-5 wow fadeIn"
+        data-wow-delay="0.1s"
+      >
+        <div className="container py-5">
+          <div className="row g-5">
+            <div className="col-lg-3 col-md-6">
+              <h4 className="text-primary mb-4">Our Office</h4>
+              <p className="mb-2">
+                <i className="fa fa-map-marker-alt text-primary me-3"></i>123
+                Street, New York, USA
+              </p>
+              <p className="mb-2">
+                <i className="fa fa-phone-alt text-primary me-3"></i>+012 345
+                67890
+              </p>
+              <p className="mb-2">
+                <i className="fa fa-envelope text-primary me-3"></i>
+                info@example.com
+              </p>
+              <div className="d-flex pt-3">
+                <a
+                  className="btn btn-square btn-primary rounded-circle me-2"
+                  href=""
+                >
+                  <i className="fab fa-twitter"></i>
+                </a>
+                <a
+                  className="btn btn-square btn-primary rounded-circle me-2"
+                  href=""
+                >
+                  <i className="fab fa-facebook-f"></i>
+                </a>
+                <a
+                  className="btn btn-square btn-primary rounded-circle me-2"
+                  href=""
+                >
+                  <i className="fab fa-youtube"></i>
+                </a>
+                <a
+                  className="btn btn-square btn-primary rounded-circle me-2"
+                  href=""
+                >
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+              </div>
             </div>
-
-
-        </footer>
-    )
+            <div className="col-lg-3 col-md-6">
+              <h4 className="text-primary mb-4">Quick Links</h4>
+              <Link to={`/`} className="btn btn-link">
+                <i className="fa fa-angle-right text-primary me-3"></i> Home
+              </Link>
+              <Link to={`/about`} className="btn btn-link">
+                <i className="fa fa-angle-right text-primary me-3"></i> About
+              </Link>
+              <Link to={`/profile`} className="btn btn-link">
+                <i className="fa fa-angle-right text-primary me-3"></i> Profile
+              </Link>
+              <Link to={`/course`} className="btn btn-link">
+                <i className="fa fa-angle-right text-primary me-3"></i> Course
+              </Link>
+              <Link to={`/login`} className="btn btn-link">
+                <i className="fa fa-angle-right text-primary me-3"></i> Login
+              </Link>
+              <Link to={`/contact`} className="btn btn-link">
+                <i className="fa fa-angle-right text-primary me-3"></i> Contact
+              </Link>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <h4 className="text-primary mb-4">Business Hours</h4>
+              <p className="mb-1">Monday - Friday</p>
+              <h6 className="text-light">09:00 am - 07:00 pm</h6>
+              <p className="mb-1">Saturday</p>
+              <h6 className="text-light">09:00 am - 12:00 pm</h6>
+              <p className="mb-1">Sunday</p>
+              <h6 className="text-light">Closed</h6>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <h4 className="text-primary mb-4">Newsletter</h4>
+              <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+              <div className="position-relative w-100">
+                <input
+                  className="form-control bg-transparent w-100 py-3 ps-4 pe-5"
+                  type="text"
+                  placeholder="Your email"
+                />
+                <button
+                  type="button"
+                  className="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2"
+                >
+                  SignUp
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
