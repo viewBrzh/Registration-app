2f667ee022899422dea0c7511ebb2141cd24def4
Hanafi Yaming <142217119+jhin3tytwoo@users.noreply.github.com>
2024-04-18 22:34:18 +0700

    added: dashboard

--- registration-app/src/pages/dashboard.js
+++ registration-app/src/pages/dashboard.js
@@ -108,47 +108,47 @@ function Dashboard() {
 
   return (
     <Main>
-      <div class="cardBox">
-        <div class="carddash">
+      <div className="cardBox">
+        <div className="carddash">
           <div>
-            <div class="numbers">1,504</div>
-            <div class="cardName">Daily Views</div>
+            <div className="numbers">1,504</div>
+            <div className="cardName">Daily Views</div>
           </div>
 
-          <div class="iconBx">
+          <div className="iconBx">
             <ion-icon name="eye-outline"></ion-icon>
           </div>
         </div>
 
-        <div class="carddash">
+        <div className="carddash">
           <div>
-            <div class="numbers">80</div>
-            <div class="cardName">Sales</div>
+            <div className="numbers">80</div>
+            <div className="cardName">Sales</div>
           </div>
 
-          <div class="iconBx">
+          <div className="iconBx">
             <ion-icon name="cart-outline"></ion-icon>
           </div>
         </div>
 
-        <div class="carddash">
+        <div className="carddash">
           <div>
-            <div class="numbers">284</div>
-            <div class="cardName">Comments</div>
+            <div className="numbers">284</div>
+            <div className="cardName">Comments</div>
           </div>
 
-          <div class="iconBx">
+          <div className="iconBx">
             <ion-icon name="chatbubbles-outline"></ion-icon>
           </div>
         </div>
 
-        <div class="carddash">
+        <div className="carddash">
           <div>
-            <div class="numbers">$7,842</div>
-            <div class="cardName">Earning</div>
+            <div className="numbers">$7,842</div>
+            <div className="cardName">Earning</div>
           </div>
 
-          <div class="iconBx">
+          <div className="iconBx">
             <ion-icon name="cash-outline"></ion-icon>
           </div>
         </div>
@@ -165,6 +165,232 @@ function Dashboard() {
             id="myChart1"
             style={{ width: "100%", height: "100%" }}
           ></canvas>
+        </div>
+      </div>
+
+      <div className="details">
+        <div className="recentOrders">
+          <div className="cardHeader">
+            <h2
+              style={{
+                color: "#E60073",
+              }}
+            >
+              Recent Orders
+            </h2>
+            <a
+              href="#"
+              style={{
+                padding: "10px",
+                border: "none",
+                borderRadius: "5px",
+                backgroundColor: "#E60073",
+                color: "#fff",
+                cursor: "pointer",
+                textAlign: "center",
+              }}
+            >
+              View All
+            </a>
+          </div>
+
+          <table>
+            <thead>
+              <tr>
+                <td>Name</td>
+                <td>Price</td>
+                <td>Payment</td>
+                <td>Status</td>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>Star Refrigerator</td>
+                <td>$1200</td>
+                <td>Paid</td>
+                <td>
+                  <span className="status delivered">Delivered</span>
+                </td>
+              </tr>
+
+              <tr>
+                <td>Dell Laptop</td>
+                <td>$110</td>
+                <td>Due</td>
+                <td>
+                  <span className="status pending">Pending</span>
+                </td>
+              </tr>
+
+              <tr>
+                <td>Apple Watch</td>
+                <td>$1200</td>
+                <td>Paid</td>
+                <td>
+                  <span className="status return">Return</span>
+                </td>
+              </tr>
+
+              <tr>
+                <td>Addidas Shoes</td>
+                <td>$620</td>
+                <td>Due</td>
+                <td>
+                  <span className="status inProgress">In Progress</span>
+                </td>
+              </tr>
+
+              <tr>
+                <td>Star Refrigerator</td>
+                <td>$1200</td>
+                <td>Paid</td>
+                <td>
+                  <span className="status delivered">Delivered</span>
+                </td>
+              </tr>
+
+              <tr>
+                <td>Dell Laptop</td>
+                <td>$110</td>
+                <td>Due</td>
+                <td>
+                  <span className="status pending">Pending</span>
+                </td>
+              </tr>
+
+              <tr>
+                <td>Apple Watch</td>
+                <td>$1200</td>
+                <td>Paid</td>
+                <td>
+                  <span className="status return">Return</span>
+                </td>
+              </tr>
+
+              <tr>
+                <td>Addidas Shoes</td>
+                <td>$620</td>
+                <td>Due</td>
+                <td>
+                  <span className="status inProgress">In Progress</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        {/* ================= New Customers ================ */}
+        <div className="recentCustomers">
+          <div className="cardHeader">
+            <h2>Recent Customers</h2>
+          </div>
+
+          <table>
+            <tr>
+              <td width="60px">
+                <div className="imgBx">
+                  <img src="img\profile\profile_test.jpg" alt="" />
+                </div>
+              </td>
+              <td>
+                <h4>
+                  David <br /> <span>Italy</span>
+                </h4>
+              </td>
+            </tr>
+
+            <tr>
+              <td width="60px">
+                <div className="imgBx">
+                  <img src="img\profile\profile_test.jpg" alt="" />
+                </div>
+              </td>
+              <td>
+                <h4>
+                  Amit <br /> <span>India</span>
+                </h4>
+              </td>
+            </tr>
+
+            <tr>
+              <td width="60px">
+                <div className="imgBx">
+                  <img src="img\profile\profile_test.jpg" alt="" />
+                </div>
+              </td>
+              <td>
+                <h4>
+                  David <br /> <span>Italy</span>
+                </h4>
+              </td>
+            </tr>
+
+            <tr>
+              <td width="60px">
+                <div className="imgBx">
+                  <img src="img\profile\profile_test.jpg" alt="" />
+                </div>
+              </td>
+              <td>
+                <h4>
+                  Amit <br /> <span>India</span>
+                </h4>
+              </td>
+            </tr>
+
+            <tr>
+              <td width="60px">
+                <div className="imgBx">
+                  <img src="img\profile\profile_test.jpg" alt="" />
+                </div>
+              </td>
+              <td>
+                <h4>
+                  David <br /> <span>Italy</span>
+                </h4>
+              </td>
+            </tr>
+
+            <tr>
+              <td width="60px">
+                <div className="imgBx">
+                  <img src="img\profile\profile_test.jpg" alt="" />
+                </div>
+              </td>
+              <td>
+                <h4>
+                  Amit <br /> <span>India</span>
+                </h4>
+              </td>
+            </tr>
+
+            <tr>
+              <td width="60px">
+                <div className="imgBx">
+                  <img src="img\profile\profile_test.jpg" alt="" />
+                </div>
+              </td>
+              <td>
+                <h4>
+                  David <br /> <span>Italy</span>
+                </h4>
+              </td>
+            </tr>
+
+            <tr>
+              <td width="60px">
+                <div className="imgBx">
+                  <img src="img\profile\profile_test.jpg" alt="" />
+                </div>
+              </td>
+              <td>
+                <h4>
+                  Amit <br /> <span>India</span>
+                </h4>
+              </td>
+            </tr>
+          </table>
         </div>
       </div>
     </Main>
