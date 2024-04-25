0684c94417f95fd1df21ddf6e5380e132bec49b6
viewBrzh <viewsatview@gmail.com>
2024-04-25 20:33:16 +0700

    fixed: update user profile bugs

--- registration-app/src/pages/profile.js
+++ registration-app/src/pages/profile.js
@@ -8,55 +8,55 @@ import apiUrl from "../api/apiConfig";
 
 function Profile() {
   const storedUserData = localStorage.getItem("userData");
-  const [userDatas, setUserDatas] = useState({
+
+  let userData = null;
+  try {
+    userData = JSON.parse(storedUserData);
+    console.log(userData);
+  } catch (error) {
+    console.error("Failed to parse user data from localStorage");
+  }
+
+  const [userDatas, setUserDatas] = useState(userData || {
     username: "",
     email: "",
     phone: "",
     image: ""
   });
 
-  let userData = null;
-  try {
-    userData = JSON.parse(storedUserData);
-  } catch (error) {
-    console.error("Failed to parse user data from localStorage");
-  }
+  console.log(userDatas);
 
   const [showModal, setShowModal] = useState(false);
 
   const handleCloseModal = () => setShowModal(false);
   const handleShowModal = () => setShowModal(true);
 
-  useEffect(() => {
-    if (userData) {
-      setUserDatas(userData);
-    }
-  }, []);
-
   const updateUser = async (id, userData) => {
     try {
       // Update user data
       const response = await axios.put(`${apiUrl}/users/update/${id}`, userData);
       console.log(response.data);
-      
+
       // Fetch updated user data
       const getUserResponse = await axios.get(`${apiUrl}/users/${id}`);
       const updatedUserData = getUserResponse.data[0]; // Assuming response is an array with a single object
       setUserDatas(updatedUserData);
+      userData = userDatas;
+      localStorage.setItem("userData", JSON.stringify(userData));
       console.log(updatedUserData);
-  
+
       // Update local storage with updated user data
       localStorage.setItem("userData", JSON.stringify(updatedUserData));
     } catch (error) {
       console.error("Failed to update user data:", error);
     }
   };
-  
+
   const handleSaveChanges = () => {
-    updateUser(userData.user_id, userDatas);
+    console.log("updating" + userDatas);
+    updateUser(userData.user_id, userDatas); // Pass userData instead of userDatas
     handleCloseModal();
   };
-
   return (
     <Main>
       {/* Hero Section */}
@@ -68,11 +68,12 @@ function Profile() {
             <div className="custom-card custom-rounded-bottom smooth-shadow-sm">
               <div className="d-flex align-items-center justify-content-between pt-4 pb-6 px-4">
                 <div className="d-flex align-items-center">
-                  <div className="custom-avatar-xxl custom-avatar-indicators avatar-online me-2 position-relative d-flex justify-content-end align-items-end custom-mt-n10" onClick={handleShowModal}>
+                  <div className="custom-avatar-xxl custom-avatar-indicators avatar-online me-2 position-relative d-flex justify-content-center align-items-center custom-mt-n10" style={{ width: '200px', height: '200px', overflow: 'hidden' }} onClick={handleShowModal}>
                     <img
                       src={`${apiUrl}/profiles/${userDatas.image}`}
-                      className="custom-avatar-xxl rounded-circle border border-2"
+                      className="rounded-circle border border-2"
                       alt="Image"
+                      style={{ objectFit: 'cover', width: '100%', height: '100%' }}
                     />
                   </div>
                   <div className="lh-1">
@@ -135,17 +136,17 @@ function Profile() {
               <Form.Label>Username</Form.Label>
               <Form.Control type="text" placeholder="Enter username" value={userDatas.username} onChange={(e) => setUserDatas({ ...userDatas, username: e.target.value })} />
             </Form.Group>
-            <br/>
+            <br />
             <Form.Group controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
               <Form.Control type="email" placeholder="Enter email" value={userDatas.email} onChange={(e) => setUserDatas({ ...userDatas, email: e.target.value })} />
             </Form.Group>
-            <br/>
+            <br />
             <Form.Group controlId="formBasicPhone">
               <Form.Label>Phone</Form.Label>
               <Form.Control type="phone" placeholder="Enter phone number" value={userDatas.phone} onChange={(e) => setUserDatas({ ...userDatas, phone: e.target.value })} />
             </Form.Group>
-            <br/>
+            <br />
             <Form.Group controlId="formBasicImage">
               <Form.Label>Upload Image</Form.Label>
               <FileUploadProfile onFileUpload={(imageName) => setUserDatas({ ...userDatas, image: imageName })} />
