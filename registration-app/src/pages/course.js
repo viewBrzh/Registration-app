885f45cf05314b9f977cc962846db8357bc3b1e7
teentsw3421 <tassawas3421@gmail.com>
2024-05-05 14:51:31 +0700

    move location to below date and resize card course

--- registration-app/src/pages/course.js
+++ registration-app/src/pages/course.js
@@ -61,22 +61,20 @@ function Course(props) {
 
   const formatDate = (start_date, finish_date) => {
     const toBuddhistEra = (year) => {
-        return year + 543;
+      return year + 543;
     };
 
     const formatBEYear = (date) => {
-        const year = toBuddhistEra(date.getFullYear());
-        return date.toLocaleDateString("en-GB").replace(date.getFullYear(), year);
+      const year = toBuddhistEra(date.getFullYear());
+      return date.toLocaleDateString("en-GB").replace(date.getFullYear(), year);
     };
 
     if (start_date === finish_date) {
-        return formatBEYear(new Date(start_date));
+      return formatBEYear(new Date(start_date));
     } else {
-        return `${formatBEYear(new Date(start_date))} - ${formatBEYear(new Date(finish_date))}`;
+      return `${formatBEYear(new Date(start_date))} - ${formatBEYear(new Date(finish_date))}`;
     }
-};
-
-
+  };
 
   const filteredBasicCourses = basicCourses.filter((course) =>
     course.course_detail_name.toLowerCase().includes(searchQuery.toLowerCase())
@@ -89,20 +87,13 @@ function Course(props) {
   return (
     <Main>
       {/* Hero Section */}
-      <div
-        className="container-fluid page-header py-5 mb-5 wow fadeIn hero-section"
-        data-wow-delay="0.1s"
-      >
+      <div className="container-fluid page-header py-5 mb-5 wow fadeIn hero-section" data-wow-delay="0.1s">
         <div className="container text-center py-5">
-          <h1 className="display-2 text-dark mb-4 animated slideInDown">
-            Courses
-          </h1>
+          <h1 className="display-2 text-dark mb-4 animated slideInDown">Courses</h1>
           <nav aria-label="breadcrumb animated slideInDown">
             <ol className="breadcrumb justify-content-center mb-0">
               <li className="breadcrumb-item">
-                <Link to={`/`} className="breadcrumb-item">
-                  Home
-                </Link>
+                <Link to={`/`} className="breadcrumb-item">Home</Link>
               </li>
               <li className="breadcrumb-item text-dark" aria-current="page" style={{ fontWeight: 'bold'}}>
                 Courses
@@ -120,29 +111,13 @@ function Course(props) {
         <div className="row justify-content-center section">
           {filteredBasicCourses?.map((course) => (
             <div className="col-lg-3" key={course.train_course_id}>
-              <div
-                className="properties properties2 mb-30 center-div"
-                style={{
-                  height: "auto",
-                  marginBottom: "20px",
-                  position: "relative",
-                }}
-              >
-                <div
-                  className="properties__card"
-                  style={{
-                    border: "1px solid #e0e0e0",
-                    borderRadius: "10px",
-                    overflow: "hidden",
-                  }}
-                >
+              <div className="properties properties2 mb-30 center-div" style={{ height: "auto", marginBottom: "20px", position: "relative" }}>
+                <div className="properties__card" style={{ border: "1px solid #e0e0e0", borderRadius: "10px", overflow: "hidden" }}>
                   {/* Card content */}
                   <div className="properties__img overlay1">
                     <Link to={`/detail/${course.train_course_id}`}>
                       <img src={`${apiUrl}/images/${course.image}`} alt="" />
                     </Link>
-                    {/* Add bookmark button here */}
-                    
                   </div>
                   <div className="properties__caption">
                     <h5>
@@ -153,24 +128,25 @@ function Course(props) {
                       </Link>
                     </h5>
                     <p>{course.train_detail}</p>
-                    <div className="properties__footer d-flex justify-content-between align-items-center">
+                    <div className="properties__footer">
+                      
                       <div className="date" style={{ color: "blue" }}>
                         <span>
-                          {formatDate(course.start_date, course.finish_date)}
-                        </span>
-                      </div>
-                      <div className="location">
-                        <span>
-                          {course.train_place.length > 20
-                            ? `${course.train_place.substring(0, 20)}...`
-                            : course.train_place}
+                          <p> Training date :
+                            {formatDate(course.start_date, course.finish_date)}</p>
                         </span>
                       </div>
                     </div>
+                    <div className="location">
+                        <span>
+                          <p>location : 
+                          {course.train_place.length > 20
+                            ? `${course.train_place.substring(0, 25)}...`
+                            : course.train_place}</p>
+                        </span>
+                      </div>
                     <Link to={`/detail/${course.train_course_id}`}>
-                      <a href="#" className="btn card-btn">
-                        More Detail
-                      </a>
+                      <a href="#" className="btn card-btn">More Detail</a>
                     </Link>
                   </div>
                 </div>
@@ -190,22 +166,8 @@ function Course(props) {
         <div className="row justify-content-center section">
           {filteredRetreatCourses?.map((course) => (
             <div className="col-lg-3" key={course.train_course_id}>
-              <div
-                className="properties properties2 mb-30 center-div"
-                style={{
-                  height: "auto",
-                  marginBottom: "20px",
-                  position: "relative",
-                }}
-              >
-                <div
-                  className="properties__card"
-                  style={{
-                    border: "1px solid #e0e0e0",
-                    borderRadius: "5px",
-                    overflow: "hidden",
-                  }}
-                >
+              <div className="properties properties2 mb-30 center-div" style={{ height: "auto", marginBottom: "20px", position: "relative" }}>
+                <div className="properties__card" style={{ border: "1px solid #e0e0e0", borderRadius: "5px", overflow: "hidden" }}>
                   {/* Card content */}
                   <div className="properties__img overlay1">
                     <Link to={`/detail/${course.train_course_id}`}>
@@ -222,11 +184,9 @@ function Course(props) {
                       </Link>
                     </h5>
                     <p style={{ padding: 5 }}>{course.train_detail}</p>
-                    <div className="properties__footer d-flex justify-content-between align-items-center">
+                    <div className="properties__footer">
                       <div className="date" style={{ color: "blue" }}>
-                        <span>
-                          {formatDate(course.start_date, course.finish_date)}
-                        </span>
+                        <span>{formatDate(course.start_date, course.finish_date)}</span>
                       </div>
                       <div className="location">
                         <span>
@@ -237,9 +197,7 @@ function Course(props) {
                       </div>
                     </div>
                     <Link to={`/detail/${course.train_course_id}`}>
-                      <a href="#" className="btn card-btn">
-                        More Detail
-                      </a>
+                      <a href="#" className="btn card-btn">More Detail</a>
                     </Link>
                   </div>
                 </div>
@@ -251,10 +209,7 @@ function Course(props) {
       {/* Retreat Courses Section End */}
 
       <a>
-        <div
-          ref={searchWrapperRef}
-          className={`search-wrapper ${isActive ? "active" : ""}`}
-        >
+        <div ref={searchWrapperRef} className={`search-wrapper ${isActive ? "active" : ""}`}>
           <div className="input-holder">
             <input
               type="text"
@@ -274,4 +229,5 @@ function Course(props) {
     </Main>
   );
 }
+
 export default Course;
